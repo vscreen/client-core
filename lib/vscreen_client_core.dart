3c142355af5e61ea119a65f2c323a110537d941a
Lukas Herman <lherman.cs@gmail.com>
2019-01-13 11:02:05 -0500

    Added stop operation and cleaned up some codes

--- lib/vscreen_client_core.dart
+++ lib/vscreen_client_core.dart
@@ -5,9 +5,10 @@ class Operation {
   static const int auth = 0;
   static const int play = 1;
   static const int pause = 2;
-  static const int next = 3;
-  static const int add = 4;
-  static const int seek = 5;
+  static const int stop = 3;
+  static const int next = 4;
+  static const int add = 5;
+  static const int seek = 6;
 }
 
 enum Status { none }
@@ -23,6 +24,7 @@ class VScreen {
     void Function(Status status) authCallback,
     void Function(Status status) playCallback,
     void Function(Status status) pauseCallback,
+    void Function(Status status) stopCallback,
     void Function(Status status) nextCallback,
     void Function(Status status) addCallback,
     void Function(Status status) seekCallback,
@@ -42,6 +44,9 @@ class VScreen {
         case Operation.pause:
           pauseCallback(status);
           break;
+        case Operation.stop:
+          stopCallback(status);
+          break;
         case Operation.next:
           nextCallback(status);
           break;
@@ -59,6 +64,11 @@ class VScreen {
     this.channel.sink.close();
   }
 
+  void _send(Map<String, dynamic> request) {
+    var packet = json.encode(request);
+    this.channel.sink.add(packet);
+  }
+
   void auth(String password) {
     var request = {
       "operation": Operation.auth,
@@ -66,42 +76,36 @@ class VScreen {
       "password": password
     };
 
-    var packet = json.encode(request);
-    this.channel.sink.add(packet);
+    _send(request);
   }
 
   void play() {
     var request = {"operation": Operation.play};
-
-    var packet = json.encode(request);
-    this.channel.sink.add(packet);
+    _send(request);
   }
 
   void pause() {
     var request = {"operation": Operation.pause};
+    _send(request);
+  }
 
-    var packet = json.encode(request);
-    this.channel.sink.add(packet);
+  void stop() {
+    var request = {"operation": Operation.stop};
+    _send(request);
   }
 
   void next() {
     var request = {"operation": Operation.next};
-
-    var packet = json.encode(request);
-    this.channel.sink.add(packet);
+    _send(request);
   }
 
   void add(String url) {
     var request = {"operation": Operation.add, "url": url};
-
-    var packet = json.encode(request);
-    this.channel.sink.add(packet);
+    _send(request);
   }
 
   void seek(double position) {
     var request = {"operation": Operation.seek, "position": position};
-
-    var packet = json.encode(request);
-    this.channel.sink.add(packet);
+    _send(request);
   }
 }
