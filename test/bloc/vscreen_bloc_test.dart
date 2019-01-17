bf4d55f7a2d180173e57f6ef549de2fd48653afe
Lukas Herman <lherman.cs@gmail.com>
2019-01-17 09:11:38 -0500

    Added stop test

--- test/bloc/vscreen_bloc_test.dart
+++ test/bloc/vscreen_bloc_test.dart
@@ -134,4 +134,66 @@ void main() {
       expect(curState.errorReason, equals("[pause] unknown error"));
     });
   });
+
+  group('stop', () {
+    test('should add stop operation and set states properly', () async {
+      var vscreen = MockVScreen();
+      var status = Status();
+      status.code = StatusCode.OK;
+      when(vscreen.stop()).thenAnswer((_) => Future.value(status));
+
+      var vscreenBloc = VScreenBloc.test(vscreen);
+      var stateQueue = StreamQueue(vscreenBloc.vscreenState);
+
+      vscreenBloc.dispatch(Stop());
+      var curState = await stateQueue.next;
+      expect(curState.url, equals(""));
+      expect(curState.port, equals(8080));
+      expect(curState.title, equals("video title"));
+      expect(curState.thumbnailURL, equals(""));
+      // isPlaying is false because the only way to change states
+      // is through server responses.
+      expect(curState.isPlaying, equals(false));
+      expect(curState.position, equals(0.0));
+      expect(curState.errorReason, equals(""));
+    });
+
+    test('should fail with operation failed', () async {
+      var vscreen = MockVScreen();
+      var status = Status();
+      status.code = StatusCode.OPERATION_FAILED;
+      when(vscreen.stop()).thenAnswer((_) => Future.value(status));
+
+      var vscreenBloc = VScreenBloc.test(vscreen);
+      var stateQueue = StreamQueue(vscreenBloc.vscreenState);
+
+      vscreenBloc.dispatch(Stop());
+      var curState = await stateQueue.next;
+      expect(curState.url, equals(""));
+      expect(curState.port, equals(8080));
+      expect(curState.title, equals("video title"));
+      expect(curState.thumbnailURL, equals(""));
+      expect(curState.isPlaying, equals(false));
+      expect(curState.position, equals(0.0));
+      expect(curState.errorReason, equals("[stop] operation failed"));
+    });
+
+    test('should fail with unknown error', () async {
+      var vscreen = MockVScreen();
+      when(vscreen.stop()).thenAnswer((_) => throw Exception());
+
+      var vscreenBloc = VScreenBloc.test(vscreen);
+      var stateQueue = StreamQueue(vscreenBloc.vscreenState);
+
+      vscreenBloc.dispatch(Stop());
+      var curState = await stateQueue.next;
+      expect(curState.url, equals(""));
+      expect(curState.port, equals(8080));
+      expect(curState.title, equals("video title"));
+      expect(curState.thumbnailURL, equals(""));
+      expect(curState.isPlaying, equals(false));
+      expect(curState.position, equals(0.0));
+      expect(curState.errorReason, equals("[stop] unknown error"));
+    });
+  });
 }
