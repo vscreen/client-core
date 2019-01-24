e34e3819370dece5e253f4542bacc8f9af9c080a
Lukas Herman <lherman.cs@gmail.com>
2019-01-23 21:23:20 -0500

    Exported states

--- lib/vscreen_client_core.dart
+++ lib/vscreen_client_core.dart
@@ -3,3 +3,6 @@ library vscreen_client_core;
 export './src/blocs/vscreen_bloc.dart';
 export './src/blocs/connection/connection_event.dart';
 export './src/blocs/player/player_event.dart';
+export './src/blocs/connection/connection_state.dart';
+export './src/blocs/player/player_state.dart';
+export './src/blocs/error/error_state.dart';
